6e45516120d2b6b2a4e31624ddbd73c80f27d367
Alberto Mercado <albertjmrs+gitkraken@gmail.com>
2019-10-11 09:37:17 +0100

    Hello world with modular-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.scss";
+import PublicIcon from "@material-ui/icons/Public";
+import Button from "@material-ui/core/Button";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello world!</p>
+        <p>
+          <Button variant="contained" color="primary">
+            {"Hello "}
+            <span style={{ width: 8 }} />
+            <PublicIcon />
+          </Button>
+        </p>
       </header>
     </div>
   );
